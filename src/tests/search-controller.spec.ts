353598b327116dd26e80b817477c462703b6be26
Christian Schmid <cschmid@pdx.edu>
2022-01-30 21:40:38 -0800

    Added quiz/search controller test suite

---
+++ src/tests/search-controller.spec.ts
@@ -0,0 +1,10 @@
+import supertest, { SuperTest, Test } from 'supertest';
+import { app } from '../app';
+import { prisma } from '../client';
+import { Individual, User } from '@prisma/client';
+
+let request = supertest(app);
+
+describe('Quiz Endpoints', () => {
+    test("sanity check", () => expect(1+1).toBeTruthy());
+});
