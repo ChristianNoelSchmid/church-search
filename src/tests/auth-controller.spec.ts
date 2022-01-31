a785e58f3684efac2e5a7b4249ca0353d2b00a78
Christian Schmid <cschmid@pdx.edu>
2022-01-30 21:37:52 -0800

    Continued auth testing. Needs logout test / more refined refreshToken test

--- src/tests/auth-controller.spec.ts
+++ src/tests/auth-controller.spec.ts
@@ -6,6 +6,7 @@ import { Individual, User } from '@prisma/client';
 let request = supertest(app);
 
 let user: User & { indiv: Individual | undefined };
+let refreshToken: string = "";
 
 describe('Auth Endpoints', () => {
     beforeAll(async () => {
@@ -17,23 +18,55 @@ describe('Auth Endpoints', () => {
 
         user = res.body.user;
     });
-    test('POST login with valid credentials should result in a 200: return accessToken', async() => {
+    test('POST /auth/login login with valid credentials should result in a 200: return accessToken', async() => {
         const loginData = { email: 'hamilton@mail.com', password: 'password' };
 
         const res = await request.post('/auth/login').send(loginData);
         expect(res.status).toBe(200);
         expect(res.body).toHaveProperty("accessToken");
 
+        refreshToken = res.headers["set-cookie"]
+            .find((h: string) => h.includes("refreshToken"));
+        expect(refreshToken).not.toBeUndefined();
+    });
+    test('POST /auth/refresh with valid refresh token returns OK with new accessToken', async () => {
+        // The previous test will have already received a refreshToken
+        const res = await request.post('/auth/refresh')
+            .set("Cookie", refreshToken);
+        expect(res.status).toBe(200);
+
+        expect(res.body).toHaveProperty("accessToken");
+        refreshToken = res.headers["set-cookie"]
+            .find((h: string) => h.includes("refreshToken"));
+
+    });
+    test('POST /auth/refresh with used refresh token revokes current and returns 400', async () => {
+        const previousRefreshToken = refreshToken;
+        let res = await request.post('/auth/refresh')
+            .set("Cookie", refreshToken);
+
         console.log(res.headers);
 
-        expect(res.headers["set-cookie"]
-            .some((h: string) => h.includes("refreshToken")))
-            .toBeTruthy();
+        refreshToken = res.headers["set-cookie"]
+            .find((h: string) => h.includes("refreshToken"));
+
+        res = await request.post('/auth/refresh')
+            .set("Cookie", previousRefreshToken);
+        expect(res.status).toBe(400); 
+        expect(res.body).not.toHaveProperty("accessToken");
+
+        await request.post('/auth/refresh')
+            .set("Cookie", refreshToken);
+        expect(res.status).toBe(400);
+        expect(res.body).not.toHaveProperty("accessToken");
     });
     afterAll(async () => {
         if(user) {
             // After testing, delete the test User
-            await prisma.user.delete({ where: { id: user.id } });
+            await prisma.user.delete({ where: { id: user.id } })
+            await prisma.refreshToken.deleteMany({
+                where: { token: { not: "" }}
+            });
         }
     });
 });
